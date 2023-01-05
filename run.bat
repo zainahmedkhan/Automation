3a8d967a04e07bcde33e545a653c658594a2db9c
zainahmedkhan <zainahmedkhan99@gmail.com>
2023-01-05 20:15:08 +0500

    first commit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 cd/
 set JAVA_HOME=C:\Program Files\Java\jdk-11.0.14
-cd C:\Users\zain.rehani\eclipse-workspace\e-banking
+cd C:\Users\zain.rehani\eclipse-workspace\HybridFrameworkBDD
 mvn clean install
